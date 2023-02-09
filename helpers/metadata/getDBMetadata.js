5832cbeab0ddaaed62b318e938319a5779bafef5
Oleksandra-Masiuk <99670702+Oleksandra-Masiuk@users.noreply.github.com>
2023-02-09 14:28:32 +0200

    rearranged code

--- helpers/metadata/getDBMetadata.js
+++ helpers/metadata/getDBMetadata.js
@@ -1,23 +1,20 @@
 import { getColumnsFromTable, getTablesFromKeySpace, getFirstRowFromTable } from '../api/api';
 import { mapTableNames, mapColumn } from '../../mappers/mappers';
 import { validateTables } from '../validators/validateTables';
-import { getSchemaColumnData } from '../schema/getSchema';
 
 const getTableMetadata = async (client, table) => {
     const columns = await getColumnsFromTable(client, table);
     const mappedColumns = mapColumn(columns);
     const tableValues = await getFirstRowFromTable(client, table);
-    const resultedColumns = mappedColumns.map((column) => getSchemaColumnData(column, tableValues));
-
-    return { table, data: resultedColumns };
+    return { table, columns: mappedColumns, values: tableValues };
 };
 
 const getDBMetadata = async (client) => {
     const tables = await getTablesFromKeySpace(client);
     validateTables(tables);
     const tableNames = mapTableNames(tables);
-    const tableMetadata = await Promise.all(tableNames.map(async name => await getTableMetadata(client, name)));
-    return tableMetadata;
+    const DBMetadata = await Promise.all(tableNames.map(async name => await getTableMetadata(client, name)));
+    return DBMetadata;
 };
 
 export { getDBMetadata }
